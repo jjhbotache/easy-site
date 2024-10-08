8fda346e7ad624cd04e08e48972ba59b342c5cae
jjhbotache <jjhuertasbotache@gmail.com>
2024-10-07 19:39:00 -0500

    adding gunicorn

--- easy_site/settings.py
+++ easy_site/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-42n@4d+u6ah60&iqthyh^o48u$_=y8ut1wnrj@mt93&7v5)#)+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
